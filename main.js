a0bf90e89f8dd5a3c76acaf9444d7ac0fec7a686
jihyun-jeon <85624671+jihyun-jeon@users.noreply.github.com>
2021-12-14 13:51:22 +0900

    carousel 클래스화

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-// !! 재할당 되면 let,아님 const 씀
-
 import './reset.css';
 import './style.css';
 
+/*
 // [클래스 하기 전 carousel]
 let posX = 0;
 const unitWidth = 80;
@@ -29,32 +28,31 @@ nextBtnEl.addEventListener('click', () => {
 
   wrapperEl.style.transform = `translateX(${posX}px)`;
 });
+*/
 
-/*
 // [클래스화 하기]
 function Carousel(selector) {
   this.root = document.querySelector(selector);
   this.init();
+  this.bindEvent();
 }
 
 // 1.초기세팅
 Carousel.prototype.init = function () {
-  // wrapper div를 만들어 추가함
+  // wrapper div를 만듦
   const wrapperEl = document.createElement('div');
   wrapperEl.classList.add('wrapper');
-
+  // viewport div를 만듦
   const viewportEl = document.createElement('div');
   viewportEl.classList.add('viewport');
 
-  viewportEl.append(wrapperEl);
-
   // panel div들을 wrapper의 자식으로 설정
   const panels = this.root.querySelectorAll('.panel'); // 유사배열(node list로 나옴)
-  wrapperEl.append(...panels);
-
+  wrapperEl.prepend(...panels);
+  viewportEl.prepend(wrapperEl);
   this.root.prepend(viewportEl);
 
-  // control div를 생성하여 젤 밑에 넣어 줌
+  // control div에 button를 생성하여 젤 밑에 넣어 줌
   const ctlEl = document.createElement('div');
   ctlEl.classList.add('control');
 
@@ -68,11 +66,35 @@ Carousel.prototype.init = function () {
 
   ctlEl.append(prevBtnEl, nextBtnEl);
   this.root.append(ctlEl);
-
-  // console.log(this.root);
 };
 
 // 2.버튼에 이벤트 걸기
+Carousel.prototype.bindEvent = function () {
+  const wrapperEl = this.root.querySelector('.wrapper');
+  const nextBtnEl = this.root.querySelector('.nextBtn');
+  const prevBtnEl = this.root.querySelector('.prevBtn');
+  const panels = this.root.querySelectorAll('.panel');
+  const maxPosX = -80 * (panels.length - 1); // -320, -160
+  let posX = 0;
 
-const c = new Carousel('.carousel1');
-*/
+  nextBtnEl.addEventListener('click', () => {
+    posX -= 80;
+
+    if (posX < maxPosX) {
+      posX = 0;
+    }
+    wrapperEl.style.transform = `translateX(${posX}px)`;
+  });
+
+  prevBtnEl.addEventListener('click', () => {
+    posX += 80;
+
+    if (posX > 0) {
+      posX = maxPosX;
+    }
+    wrapperEl.style.transform = `translateX(${posX}px)`;
+  });
+};
+
+const c1 = new Carousel('.carousel1');
+const c2 = new Carousel('.carousel2');
